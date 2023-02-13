622d3aa94e2c725229c2a43ee923e2fc6f91a5a3
LeThanh-dev <lethanh.1701.dev@gmail.com>
2023-02-13 08:35:40 +0700

    finished project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,53 +1,14 @@
-import { SongListByCategoryComponent } from './components/song-list-by-category/song-list-by-category/song-list-by-category.component';
-import { DefaultLayoutComponent } from './layout/default-layout/default-layout.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
 import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
-import { LovedSongComponent } from './components/loved-song/loved-song.component';
-import { SearchPageComponent } from './components/search-page/search-page.component';
-import { SignInComponent } from './components/sign-in/sign-in.component';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 const routes: Routes = [
   {
     path: '',
     title: "Spotify",
-    component: DefaultLayoutComponent,
-    children: [
-      {
-        path: '',
-        title: "Spotify",
-        component: HomePageComponent,
-      },
-      {
-        path: 'search',
-        title: "Tìm kiếm | Spotify",
-        component: SearchPageComponent,
-      },
-      {
-        path:"category/:categoryID/:categoryName",
-        component:SongListByCategoryComponent
-      },
-      {
-        path: 'loved-song',
-        title: "Bài hát đã thích | Spotify",
-        component: LovedSongComponent
-      },
-    ]
+    loadChildren: () => import("./layout/default-layout/default-layout.module").then(m => m.DefaultLayoutModule)
   },
-  {
-    path: 'sign-in',
-    title: "Đăng nhập | Spotify",
-    component: SignInComponent
-  },
-  {
-    path: 'sign-up',
-    title: "Đăng ký | Spotify",
-    component: SignUpComponent
-  },
-
-
+  
   {
     path: '**',
     title: "Không tìm thấy trang",
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,48 +6,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 import { HttpClientModule } from '@angular/common/http';
-import { LovedSongComponent } from './components/loved-song/loved-song.component';
-import { SongListTemplateComponent } from './components/song-list-template/song-list-template.component';
-import { HomePageComponent } from './components/home-page/home-page.component';
-import { SearchPageComponent } from './components/search-page/search-page.component';
-import { UserComponent } from './components/user/user.component';
-import { DefaultLayoutComponent } from './layout/default-layout/default-layout.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SongNameTemplateComponent } from './components/song-list-template/song-name-template/song-name-template/song-name-template.component';
-import { PlayIconComponent } from './components/play-icon/play-icon/play-icon.component';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { SignInComponent } from './components/sign-in/sign-in.component';
-import { AlertTemplateComponent } from './components/alert-template/alert-template/alert-template.component';
-import { HomePageTemplateComponent } from './components/home-page-template/home-page-template/home-page-template.component';
-import { SongListByCategoryComponent } from './components/song-list-by-category/song-list-by-category/song-list-by-category.component';
-import { LoadingComponent } from './components/loading/loading/loading.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotFoundPageComponent,
-    LovedSongComponent,
-    SongListTemplateComponent,
-    HomePageComponent,
-    SearchPageComponent,
-    UserComponent,
-    SignUpComponent,
-    SignInComponent,
-    DefaultLayoutComponent,
-    SongNameTemplateComponent,
-    PlayIconComponent,
-    AlertTemplateComponent,
-    HomePageTemplateComponent,
-    SongListByCategoryComponent,
-    LoadingComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
